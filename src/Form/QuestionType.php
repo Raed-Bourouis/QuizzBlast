100caa59f40aced878506ca4e8c162afc5a52b8b
Raed-Bourouis <144831482+Raed-Bourouis@users.noreply.github.com>
2025-12-04 15:49:08 +0100

    Quizz controllers

---
+++ src/Form/QuestionType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Question;
+use App\Entity\Quiz;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class QuestionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('text')
+            ->add('points')
+            ->add('timeLimit')
+            ->add('mediaUrl')
+            ->add('quiz', EntityType::class, [
+                'class' => Quiz::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Question::class,
+        ]);
+    }
+}
